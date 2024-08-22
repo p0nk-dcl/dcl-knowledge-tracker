cc18e5a62a83fbf0ea09136085980702d414c1a9
Jordan Ndetcho <jordan.ndetcho.ext@suez.com>
2024-08-22 13:11:14 +0200

    update main page link

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -373,9 +373,9 @@ const Home: NextPage = () => {
           <div className="flex flex-col px-10 py-10 text-center items-center max-w-xs rounded-3xl">
             <BugAntIcon className="h-8 w-8 fill-secondary" />
             <p>
-              Tinker with your smart contract using the{" "}
-              <Link href="/debug" passHref className="link">
-                Debug Contracts
+              Browse attestations {" "}
+              <Link href="/attestation-viewer" passHref className="link">
+                Interact with the Contracts
               </Link>{" "}
               tab.
             </p>
