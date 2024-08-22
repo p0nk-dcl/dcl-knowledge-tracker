ca757dc213a6fb5ea47e1fed867307ab1c9c244c
Jordan Ndetcho <jordan.ndetcho.ext@suez.com>
2024-08-22 14:18:02 +0200

    switch icons

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -371,7 +371,7 @@ const Home: NextPage = () => {
       <div className="flex-grow w-full mt-16 px-8 py-12">
         <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
           <div className="flex flex-col px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <BugAntIcon className="h-8 w-8 fill-secondary" />
+            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
             <p>
               Browse attestations with the {" "}
               <Link href="/attestation-viewer" passHref className="link">
@@ -381,7 +381,7 @@ const Home: NextPage = () => {
             </p>
           </div>
           <div className="flex flex-col bg-gray-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+            <BugAntIcon className="h-8 w-8 fill-secondary" />
             <p>
               Explore your local transactions with the{" "}
               <Link href="/blockexplorer" passHref className="link">
